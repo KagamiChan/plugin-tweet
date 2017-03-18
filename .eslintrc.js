67f7372677e309588f64a50bb2f7e142770af52c
Kagami <moe.pascal@gmail.com>
2017-03-19 00:48:30 +0800

    add fallback for poi version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,43 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
-};
+	'extends': 'airbnb',
+	'plugins': [
+		'react',
+		'jsx-a11y',
+		'import'
+	],
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true,
+	},
+	'rules': {
+		'semi': ['error', 'never'],
+		'import/no-unresolved': [2, { ignore: ['views/.*'] }],
+    'react/jsx-filename-extension': 'off',
+	},
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.es'],
+        'paths': [__dirname],
+      },
+    },
+    'import/core-modules': [
+      'bluebird',
+      'electron',
+      'react',
+      'react-redux',
+      'redux-observers',
+      'reselect',
+      'react-bootstrap',
+      'react-fontawesome',
+      'path-extra',
+      'fs-extra',
+      'lodash',
+      'cson',
+      'react-dom',
+      'redux',
+      'semver',
+    ],
+  },
+}
---
+++ index.es
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const reactClass = () =>
+  <div>
+    <p>Not available for your poi version.</p>
+    <p>该插件不适用于您当前的 poi 版本</p>
+    <p>該插件不適用于您當前的 poi 版本</p>
+  </div>
